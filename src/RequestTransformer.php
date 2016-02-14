392dbcc4def59d12015066215939ba7845fe2c48
Mark Story <mark@mark-story.com>
2016-02-14 15:34:08 -0500

    Implement a big chunk of request transformation.
    * SERVER data.
    * POST & GET
    * FILES & COOKIES.

---
+++ src/RequestTransformer.php
@@ -0,0 +1,120 @@
+<?php
+namespace Spekkoek;
+
+use Cake\Network\Request as CakeRequest;
+use Cake\Utility\Hash;
+use Psr\Http\Message\ServerRequestInterface as PsrRequest;
+
+/**
+ * Translate and transform from CakePHP requests into PSR7 ones and
+ * back again. This is an important step for maintaining backwards compatibility
+ * with existing CakePHP applications.
+ */
+class RequestTransformer
+{
+    /**
+     * Transform a PSR7 request into a CakePHP one.
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request The PSR7 request.
+     * @return \Cake\Network\Request The transformed request.
+     */
+    public static function toCake(PsrRequest $request)
+    {
+        $post = $request->getParsedBody();
+        $files = static::getFiles($request);
+        if ($files) {
+            $post = Hash::merge($post, $files);
+        }
+
+        return new CakeRequest([
+            'query' => $request->getQueryParams(),
+            'post' => $post,
+            'cookies' => $request->getCookieParams(),
+            'environment' => $request->getServerParams(),
+            'params' => static::getParams($request),
+            'url' => '',
+            'base' => '',
+            'webroot' => '',
+            'input' => null,
+        ]);
+    }
+
+    /**
+     * Extract the routing parameters out of the request object.
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request The request to extract params from.
+     * @return array The routing parameters.
+     */
+    protected static function getParams($request)
+    {
+        $attributes = $request->getAttributes();
+        $attributes += ['params' => []];
+        $attributes['params'] += [
+            'plugin' => null,
+            'controller' => null,
+            'action' => null,
+            '_ext' => null,
+            'pass' => []
+        ];
+        return $attributes['params'];
+    }
+
+    /**
+     * Extract the uploaded files out of the request object.
+     *
+     * CakePHP expects to get arrays of file information and
+     * not the parsed objects that PSR7 requests contain. Downsample the data here.
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request The request to extract files from.
+     * @return array The routing parameters.
+     */
+    protected static function getFiles($request)
+    {
+        return static::convertFiles([], $request->getUploadedFiles());
+    }
+
+    /**
+     * Convert a nested array of files to arrays.
+     *
+     * @param array $data The data to add files to.
+     * @param array $files The file objects to convert.
+     * @param string $path The current array path.
+     * @return array Converted file data
+     */
+    protected static function convertFiles($data, $files, $path = '')
+    {
+        foreach ($files as $key => $file) {
+            $newPath = $path;
+            if ($newPath === '') {
+                $newPath = $key;
+            }
+            if ($newPath !== $key) {
+                $newPath .= '.' . $key;
+            }
+
+            if (is_array($file)) {
+                $data = static::convertFiles($data, $file, $newPath);
+            } else {
+                $data = Hash::insert($data, $newPath, static::convertFile($file));
+            }
+        }
+        return $data;
+    }
+
+    /**
+     * Convert a single file back into an array.
+     *
+     * @param \Psr\Http\Message\UploadedFileInterface $file The file to convert.
+     * @return array
+     */
+    protected static function convertFile($file)
+    {
+        return [
+            'name' => $file->getClientFilename(),
+            'type' => $file->getClientMediaType(),
+            'tmp_name' => $file->getStream()->getMetadata('uri'),
+            'error' => $file->getError(),
+            'size' => $file->getSize(),
+        ];
+    }
+}
