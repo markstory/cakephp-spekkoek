74bf9c6c220e4f98bc78bfa38d381032b11aa2fd
Mark Story <mark@mark-story.com>
2016-02-18 22:01:06 -0500

    Add notes for future me.

--- src/BaseApplication.php
+++ src/BaseApplication.php
@@ -16,10 +16,13 @@ abstract class BaseApplication
 
     public function __invoke($request, $response, $next)
     {
-        // Convert the request/response to CakePHP equivalents.
-        // Create the application Dispatcher.
-        // Dispatch the request/response
-        // Convert the response back into a PSR7 object.
+        // - Convert the request/response to CakePHP equivalents.
+        // - Create the application Dispatcher.
+        // - Dispatch the request/response
+        // - Convert the response back into a PSR7 object.
+        // - Use the request to get the http_range and set the content-range header
+        //   if appropriate for file responses.
+        // - Return a response that Server can emit with SapiEmitter.
         return $response;
     }
 }
