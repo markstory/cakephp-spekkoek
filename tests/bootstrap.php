c2641ba5f7ec857b690a94444c78da4c803756f6
Mark Story <mark@mark-story.com>
2016-03-26 12:34:02 -0400

    Clean up and fix test bootstrapping.
    In some installs vendor/autoload.php was not being automatically
    included.
    
    Fixes #6

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,24 +1,8 @@
 <?php
 /**
  * Test suite bootstrap for LayerCake.
- *
- * This function is used to find the location of CakePHP whether CakePHP
- * has been installed as a dependency of the plugin, or the plugin is itself
- * installed as a dependency of an application.
  */
-$findRoot = function ($root) {
-    do {
-        $lastRoot = $root;
-        $root = dirname($root);
-        if (is_dir($root . '/vendor/cakephp/cakephp')) {
-            return $root;
-        }
-    } while ($root !== $lastRoot);
-
-    throw new Exception("Cannot find the root of the application, unable to run tests");
-};
-$root = $findRoot(__FILE__);
-unset($findRoot);
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Cake\Core\Configure;
 
